4689af285a7c61e6b9c7f3d2031c92c76f2e3d6f
anjdroid <anjdroid>
2019-05-03 15:47:23 +0200

    Xpath for rtvslo + overstock

--- dataextraction.py
+++ dataextraction.py
@@ -1,10 +1,18 @@
 # Import libraries
 import requests
 import re
+from lxml import html
+import json
 
+page1 = 'WebPages/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html'
+page2 = 'WebPages/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljs╠îe v razredu - RTVSLO.si.html'
+page3 = 'WebPages/overstock.com/jewelry01.html'
+page4 = 'WebPages/overstock.com/jewelry02.html'
+page5 = 'WebPages/hm/Dresses _ Maxi, Party, Evening & More _ H&M GB.html'
+page6 = 'WebPages/ebay/headphones _ eBay.html'
 
 # We rather use the locally-cached file as it may have changed online.
-pageContent = open('WebPages\\rtvslo.si\Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html', 'r').read()
+pageContent = open('WebPages/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html', 'r').read()
 print("Page content:\n'%s'." % pageContent)
 
 # Get the article date
@@ -19,3 +27,114 @@ print("Found title: '%s'." % title)
 matches = re.finditer(regex, pageContent)
 for match in matches:
     print("Found date: '{}'.".format( match.group(0) ))
+
+
+################# XPATH THINGS
+
+def openPageContent(page):
+	return open(page, 'rb').read()
+
+def xpathForRtvSlo(page):
+	# TODO: additionally clean strings
+	# TODO: extract content!!
+	# TODO: better Xpaths?? skrajsat??
+	
+	# download page
+	pageContent = openPageContent(page)
+	tree = html.fromstring(pageContent)
+
+	# author
+	author = str(tree.xpath('//*[@id="main-container"]/div[position()=3]/div/div[1]/div[1]/div/text()')[0])
+	print("Found author: ", author)
+
+	# published time
+	publishedTime = str(tree.xpath('//*[@id="main-container"]/div[position()=3]/div/div[1]/div[position()=2]/text()')[0])
+	print("Found publishedTime: '%s'." % publishedTime)
+
+	# title
+	title = str(tree.xpath('//*[@id="main-container"]/div[position()=3]/div/header/h1/text()')[0])
+	print("Found title: '%s'." % title)
+
+	# subtitle
+	subTitle = str(tree.xpath('//*[@id="main-container"]/div[position()=3]/div/header/div[position()=2]/text()')[0])
+	print("Found subtitle: '%s'." % subTitle)
+
+	# lead
+	lead = str(tree.xpath('//*[@id="main-container"]/div[position()=3]/div/header/p/text()')[0])
+	print("Found lead: '%s'." % lead)
+
+	# TODO: extract content properly
+	content = str(tree.xpath('//*[@id="main-container"]/div[position()=3]/div/div[position()=2]')[0])
+	print("Found content: '%s'." % content)
+
+	# TODO: Additionally format date
+	# date = re.sub(r"\s", "", date)
+	# print("Found date: '%s'." % date)
+
+	# Extract the image URL
+	#imageUrl = str(tree.xpath('//*[@id="container"]/div/div[1]/div[1]/div[1]/div/div[2]/a/img/@src')[0])
+	#print("Found imageUrl: '%s'." % imageUrl)
+
+	dataItem = {
+	    "Author": author,
+	    "PublishedTime": publishedTime,
+	    "Title": title,
+	    "SubTitle" : subTitle,
+	    "Lead" : lead,
+	    "Content" : content
+	}
+
+	print("Output object:\n%s" % json.dumps(dataItem, indent = 4))
+
+xpathForRtvSlo(page1)
+xpathForRtvSlo(page2)
+
+def xpathForOverstock(page):
+	# TODO: additionally clean strings
+	# TODO: shorten xpaths!!!!!
+	
+	# download page
+	pageContent = openPageContent(page)
+	tree = html.fromstring(pageContent)
+
+	# Title
+	title = str(tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/a/b/text()')[0])
+	print("Found title: ", title)
+
+	# Content
+	content = str(tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[2]/span/text()')[0])
+	print("Found content: '%s'." % content)
+
+	# ListPrice
+	listprice = str(tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s/text()')[0])
+	print("Found listprice: '%s'." % listprice)
+
+	# Price
+	price = str(tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/span/b/text()')[0])
+	print("Found price: '%s'." % price)
+
+	# Saving + saving percent
+	save = str(tree.xpath('/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span/text()')[0])
+	saving = save.split('(')[0].strip(" ")
+	print("Found saving: '%s'." % saving)
+
+	# SavingPercent
+	savingpercent = save.split('(')[1].strip(" )")
+
+	dataItem = {
+	    "Title": title,
+	    "Content": content,
+	    "ListPrice": listprice,
+	    "Price" : price,
+	    "Saving" : saving,
+	    "SavingPercent" : savingpercent
+	}
+
+	print("Output object:\n%s" % json.dumps(dataItem, indent = 4))
+
+xpathForRtvSlo(page1)
+xpathForRtvSlo(page2)
+
+xpathForOverstock(page3)
+xpathForOverstock(page4)
+
